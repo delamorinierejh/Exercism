656e8c2ff62911c46237fbad147e6ea4cc67ba2e
delamorinierejh <delamorinierejh@gmail.com>
2016-11-15 18:05:40 +0000

    pushing rna transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,7 +1,16 @@
+module BookKeeping
+    VERSION = 4
+end
+
 class Complement
- DNA = 'GCTA'
- RNA = 'CGAU'
+ DNA = ['G','C','T','A']
+ RNA = ['C','G','A','U']
   def self.of_dna(dna)
-    dna.split.map { |letter| RNA.index(DNA.index(letter))}.join
+    dna.chars.map { |letter| 
+      if (DNA.index(letter) != nil)
+      RNA[DNA.index(letter)]
+    else return ""
+    end
+      }.join
   end
 end
