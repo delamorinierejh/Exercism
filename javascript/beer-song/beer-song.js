27bc27e61d691e6ab6afd00b7947d67093c72a88
delamorinierejh <delamorinierejh@gmail.com>
2016-12-01 15:39:07 +0000

    updated to es6

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -7,9 +7,9 @@ function BeerSong(){
     pluralTwo = number === 1 ? 's' : pluralTwo;
     let secondNum = number > 1 ? (number-1) : "no more";
     let itOne = number > 1 ? "one" : "it";
-    var firstBit = `${number} bottle${pluralOne} of beer on the wall, ${typeof number === 'number' ? number : number.toLowerCase()} bottle${pluralOne} of beer.\n`
+    let firstBit = `${number} bottle${pluralOne} of beer on the wall, ${typeof number === 'number' ? number : number.toLowerCase()} bottle${pluralOne} of beer.\n`
 
-    var secondBit = `Take ${itOne} down and pass it around, ${secondNum} bottle${pluralTwo} of beer on the wall.\n`;
+    let secondBit = `Take ${itOne} down and pass it around, ${secondNum} bottle${pluralTwo} of beer on the wall.\n`;
 
     secondBit = number === 'No more' ? 'Go to the store and buy some more, 99 bottles of beer on the wall.\n' : secondBit;
 
@@ -17,7 +17,7 @@ function BeerSong(){
   }
 
   this.sing = function(verse1, verse2=0){
-    var array = [];
+    let array = [];
     while (verse1 >= verse2){
       array.push(this.verse(verse1));
       verse1--;
