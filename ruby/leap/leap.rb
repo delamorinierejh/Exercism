7cddf14943879978f018f86d36132ee80fe6f7c4
delamorinierejh <delamorinierejh@gmail.com>
2017-02-13 14:22:28 +0000

    submitting ruby leap

---
+++ ruby/leap/leap.rb
@@ -0,0 +1,16 @@
+module BookKeeping
+  VERSION = 2
+end
+
+class Year
+
+  def self.leap?(year)
+
+    return true if year%400==0
+    return false if year%100==0
+    return true if year%4==0
+    return false
+    
+  end
+
+end
